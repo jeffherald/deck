47c2c2663f78a12b75098b884061e4da470227c6
Jeff Herald <jeff.herald@uber.com>
2020-12-09 18:25:58 -0500

    WIP commit before move

--- app/scripts/modules/core/src/kubernetes/component/group/RawResource.tsx
+++ app/scripts/modules/core/src/kubernetes/component/group/RawResource.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { UISref, UISrefActive } from '@uirouter/react';
 import './RawResource.less';
 
 interface IRawResourceProps {
@@ -12,27 +13,44 @@ export class RawResource extends React.Component<IRawResourceProps, IRawResource
     super(props);
   }
 
+  private handleRawResourceClicked = (event: React.MouseEvent<any>) => {
+    //ReactGA.event({ category: 'Cluster Pod', action: 'Load Server Group Details' });
+    //this.loadDetails(event);
+  };
+
   public render() {
+    const key = this.props.resource.kind + '-' + this.props.resource.displayName + '-' + this.props.resource.namespace;
+    const params = {
+      account: this.props.resource.account,
+      apiVersion: this.props.resource.apiVersion,
+      kind: this.props.resource.kind,
+      namespace: this.props.resource.namespace,
+      displayName: this.props.resource.displayName,
+    };
     return (
-      <div className="raw-resource-card">
-        <h4 className="raw-resource-title">
-          {this.props.resource.kind}: <b>{this.props.resource.displayName}</b>
-        </h4>
-        <div className="raw-resource-details" style={{ display: 'flex' }}>
-          <div className="raw-resource-details-column" style={{ display: 'flex' }}>
-            <div className="raw-resource-details-column-label">account:</div>
-            <div>{this.props.resource.account}</div>
+      <UISrefActive class="active">
+        <UISref to=".rawResourceDetails" params={params}>
+          <div id={key} className="raw-resource-card clickable clickable-row" onClick={this.handleRawResourceClicked}>
+            <h4 className="raw-resource-title">
+              {this.props.resource.kind}: <b>{this.props.resource.displayName}</b>
+            </h4>
+            <div className="raw-resource-details" style={{ display: 'flex' }}>
+              <div className="raw-resource-details-column" style={{ display: 'flex' }}>
+                <div className="raw-resource-details-column-label">account:</div>
+                <div>{this.props.resource.account}</div>
+              </div>
+              <div className="raw-resource-details-column" style={{ display: 'flex' }}>
+                <div className="raw-resource-details-column-label">namespace:</div>
+                <div>{this.props.resource.namespace}</div>
+              </div>
+              <div className="raw-resource-details-column" style={{ display: 'flex' }}>
+                <div className="raw-resource-details-column-label">apiVersion:</div>
+                <div>{this.props.resource.apiVersion}</div>
+              </div>
+            </div>
           </div>
-          <div className="raw-resource-details-column" style={{ display: 'flex' }}>
-            <div className="raw-resource-details-column-label">namespace:</div>
-            <div>{this.props.resource.namespace}</div>
-          </div>
-          <div className="raw-resource-details-column" style={{ display: 'flex' }}>
-            <div className="raw-resource-details-column-label">apiVersion:</div>
-            <div>{this.props.resource.apiVersion}</div>
-          </div>
-        </div>
-      </div>
+        </UISref>
+      </UISrefActive>
     );
   }
 }
